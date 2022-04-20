311cba341fd59936e3ac0954defbf13f1d26a89c
sankarshan <sankarshan@pobox.com>
2022-04-20 09:48:52 +0530

    Health Records and Data Privacy
    added topic maps for Health Records and Data Privacy

---
+++ Health Records and Data Privacy.mm
@@ -0,0 +1,287 @@
+
+<map version="0.9.0">
+    <node TEXT="Health Records and Data Privacy" FOLDED="false" POSITION="right" ID="6242bf23473ea5ba732faccb" X_COGGLE_POSX="0" X_COGGLE_POSY="0">
+        <edge COLOR="#b4b4b4"/>
+        <font NAME="Helvetica" SIZE="17"/>
+        <node TEXT="Available legal framework" FOLDED="false" POSITION="left" ID="1fb0b12beefaf20722ded264">
+            <edge COLOR="#7aa3e5"/>
+            <font NAME="Helvetica" SIZE="15"/>
+            <node TEXT="Remote delivery of health services" FOLDED="false" POSITION="left" ID="ccdc5d51c46a070a1d964079">
+                <edge COLOR="#83abe8"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="Contrast with other nations" FOLDED="false" POSITION="left" ID="0718f13d791fbf09a5c088e8">
+                <edge COLOR="#83abe6"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="Gaps" FOLDED="false" POSITION="left" ID="8a07455926a5e0786527a161">
+                <edge COLOR="#81a9e4"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="Uniformity" FOLDED="false" POSITION="left" ID="e19b2ad4fd905ebfc4380d0f">
+                <edge COLOR="#82a9e5"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="Compliance" FOLDED="false" POSITION="left" ID="91f1d36153296a5af9e76e65">
+                <edge COLOR="#85ace6"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="Enforcement" FOLDED="false" POSITION="left" ID="06064902bcf286b76a67e372">
+                <edge COLOR="#73a1e6"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="Audit and Controls" FOLDED="false" POSITION="left" ID="5aa3b65410ff9cf1cb0ce7af">
+                <edge COLOR="#85ace6"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="Proposals in the pipeline" FOLDED="false" POSITION="left" ID="5567657b12ccb6a69cb14e77">
+                <edge COLOR="#7ea8e7"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="Exceptional Circumstances" FOLDED="false" POSITION="left" ID="82697aeff6cce5c55dbadf25">
+                <edge COLOR="#72a0e5"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+        </node>
+        <node TEXT="Data" FOLDED="false" POSITION="right" ID="ea8091a1cb84366f0ba02549">
+            <edge COLOR="#988ee3"/>
+            <font NAME="Helvetica" SIZE="15"/>
+            <node TEXT="Types of data" FOLDED="false" POSITION="right" ID="23df02da6f1cb39a11987dbf">
+                <edge COLOR="#968ee1"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="Interoperability" FOLDED="false" POSITION="right" ID="17f07a24d739392ab7b13ddc">
+                <edge COLOR="#968ee1"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="Portability" FOLDED="false" POSITION="right" ID="2c14e6649dc9813da3ca32d3">
+                <edge COLOR="#8f86e3"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="Cost of compliance" FOLDED="false" POSITION="right" ID="07aff61a4069a51c9e7c985c">
+                <edge COLOR="#948be2"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="Authorization" FOLDED="false" POSITION="right" ID="36ac0704c3110efedc0b38d7">
+                <edge COLOR="#9a91e4"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="Data Storage and Retention" FOLDED="false" POSITION="right" ID="27036b4524d1eb365857456b">
+                <edge COLOR="#958de2"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="Data Input" FOLDED="false" POSITION="right" ID="28c8e1ef160b4ebfb9d9c93e">
+                <edge COLOR="#938ae1"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="Data Processing" FOLDED="false" POSITION="right" ID="9bfc1a34c86bc7cf5e62eea3">
+                <edge COLOR="#968ee1"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="Access" FOLDED="false" POSITION="right" ID="042400cb012455c2398f8b1d">
+                <edge COLOR="#9289e2"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="Retention complexities" FOLDED="false" POSITION="right" ID="bccbff3d3fcc35b90b9ee360">
+                <edge COLOR="#9087e0"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="Sources of data" FOLDED="false" POSITION="right" ID="998f7da62ad72b1cbb70822b">
+                <edge COLOR="#9890e3"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="Formats and standards" FOLDED="false" POSITION="right" ID="3b4e313d41de5c2bb50379f1">
+                <edge COLOR="#8f86e1"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="Data security" FOLDED="false" POSITION="right" ID="f29ad9642d84b92ff365a8f1">
+                <edge COLOR="#958de1"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+        </node>
+        <node TEXT="Ecosystem" FOLDED="false" POSITION="right" ID="f6b305c3b92a4e49c04ad617">
+            <edge COLOR="#67d7c4"/>
+            <font NAME="Helvetica" SIZE="15"/>
+            <node TEXT="Telemedicine" FOLDED="false" POSITION="right" ID="39c427d256770a758ad38bec">
+                <edge COLOR="#67d7c2"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="Open Data Systems" FOLDED="false" POSITION="right" ID="ca08b11cd1b39416fa99cd7c">
+                <edge COLOR="#61d3be"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="Public Health Policy" FOLDED="false" POSITION="right" ID="ddd6848cbd3f41e84ef23fff">
+                <edge COLOR="#5fd7c1"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="Apps" FOLDED="false" POSITION="right" ID="2647abf7ef224dedd60a1f94">
+                <edge COLOR="#62d8c2"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="Insurance" FOLDED="false" POSITION="right" ID="b2da30768ae4507697be037e">
+                <edge COLOR="#5ed4be"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="Financing" FOLDED="false" POSITION="right" ID="d24cd63feb7c85a984159771">
+                <edge COLOR="#6cd6c2"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="Devices" FOLDED="false" POSITION="right" ID="1c668b706fe7786352da7739">
+                <edge COLOR="#6bd9c4"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="Clinical Policy" FOLDED="false" POSITION="right" ID="ca8ea08bcc9fa6d2db2aa96c">
+                <edge COLOR="#62d6c0"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+        </node>
+        <node TEXT="Outputs and Outcomes" FOLDED="false" POSITION="left" ID="faa3a3e54ccaeb0e9e7c4899">
+            <edge COLOR="#efa670"/>
+            <font NAME="Helvetica" SIZE="15"/>
+            <node TEXT="Recommendations to GoI stakeholders" FOLDED="false" POSITION="left" ID="bbe9df78bbb9d214c3201fb8">
+                <edge COLOR="#f0a774"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="Interim report on present state (Indian context)" FOLDED="false" POSITION="left" ID="bbab93123f744e973422e1ea">
+                <edge COLOR="#f1a876"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="Talks from experts" FOLDED="false" POSITION="left" ID="aca151fd8f6e2aa48f422ef8">
+                <edge COLOR="#efa36e"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="Discussions with stakeholders" FOLDED="false" POSITION="left" ID="24afea134b19aa398ae62cf2">
+                <edge COLOR="#eca06b"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="Data Privacy Design survey with startups" FOLDED="false" POSITION="left" ID="a487189274febd56e4ddcbe4">
+                <edge COLOR="#f0a775"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+        </node>
+        <node TEXT="Stakeholders" FOLDED="false" POSITION="left" ID="b3971ad0fd9a38e74bb69016">
+            <edge COLOR="#ebd95f"/>
+            <font NAME="Helvetica" SIZE="15"/>
+            <node TEXT="Research divisions" FOLDED="false" POSITION="left" ID="2ae52991d47a389145e412f5">
+                <edge COLOR="#edd965"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="Emergency Services" FOLDED="false" POSITION="left" ID="b52936e17496285b7feb54f7">
+                <edge COLOR="#ecd552"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="Public Health experts" FOLDED="false" POSITION="left" ID="6b19c967507428438c6e736d">
+                <edge COLOR="#ebd763"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="Application and Services Developers" FOLDED="false" POSITION="left" ID="186f5f12886e449b2b58b6f5">
+                <edge COLOR="#edd969"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="VCs investing in healthcare" FOLDED="false" POSITION="left" ID="c90811613e540dceae78c6c2">
+                <edge COLOR="#e9d45b"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="Healthcare beat journalists" FOLDED="false" POSITION="left" ID="c13c9a569801884022ab6a49">
+                <edge COLOR="#ead766"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="Healthcare Providers" FOLDED="false" POSITION="left" ID="f0914ce341cebf33d8f91de6">
+                <edge COLOR="#ecd96a"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="Legal Experts" FOLDED="false" POSITION="left" ID="e730838837c6b2152add0728">
+                <edge COLOR="#edd757"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="Ethicists" FOLDED="false" POSITION="left" ID="925d38b48eb1d228143ea695">
+                <edge COLOR="#edda66"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="Policy Analysts" FOLDED="false" POSITION="left" ID="74c97e7ccd843b28610fb124">
+                <edge COLOR="#ebd659"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="Rights activists" FOLDED="false" POSITION="left" ID="80f25fd2103108f248aabff7">
+                <edge COLOR="#ebd75f"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="Data science experts" FOLDED="false" POSITION="left" ID="09f36101299f71bbc580a170">
+                <edge COLOR="#ebd761"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="Grant making foundations" FOLDED="false" POSITION="left" ID="79da6f62d3852a073ddd2b3d">
+                <edge COLOR="#ead454"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+        </node>
+        <node TEXT="Topics of Concern" FOLDED="false" POSITION="right" ID="45274bb84e40fa2cd469e9bb">
+            <edge COLOR="#9ed56b"/>
+            <font NAME="Helvetica" SIZE="15"/>
+            <node TEXT="Training data sets" FOLDED="false" POSITION="right" ID="5df0fbf70fbea4ef64a34dc2">
+                <edge COLOR="#a3d76f"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="Data ownership and control" FOLDED="false" POSITION="right" ID="1d1b865631f05b160efd238f">
+                <edge COLOR="#9dd268"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="Digital Health Credentials" FOLDED="false" POSITION="right" ID="1c8cef469f5b87abaccb8772">
+                <edge COLOR="#9dd466"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="Digital Identifiers" FOLDED="false" POSITION="right" ID="a3e862656cb7d3108f89b013">
+                <edge COLOR="#9dd367"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="Access to data / Open data framework" FOLDED="false" POSITION="right" ID="fdf093b9843ff7719ee8cd8b">
+                <edge COLOR="#9dd565"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="Understanding Harms" FOLDED="false" POSITION="right" ID="0c5c34a30c8b539a15c6cc46">
+                <edge COLOR="#a5d574"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="Equity in infrastructure" FOLDED="false" POSITION="right" ID="4d05da83c728683a41294657">
+                <edge COLOR="#a1d86a"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="Awareness around Privacy" FOLDED="false" POSITION="right" ID="4673a4a15f71ec91a7c0911a">
+                <edge COLOR="#9ad460"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="Trust in Health System" FOLDED="false" POSITION="right" ID="f944f3658b1c0d51420d0710">
+                <edge COLOR="#9bd561"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="Consent (access/sharing)" FOLDED="false" POSITION="right" ID="75bc0883ee42903bd3385970">
+                <edge COLOR="#9dd367"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="Dangers, Risks, Potential Harms and Threats (DRPhT)" FOLDED="false" POSITION="right" ID="53d4e77ec8a8702d028c623d">
+                <edge COLOR="#a3d571"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="Surveillance" FOLDED="false" POSITION="right" ID="2fb35b39844e449883abd26b">
+                <edge COLOR="#a1d46e"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="Centralized data stores" FOLDED="false" POSITION="right" ID="d7483723aee5cb519ffb8ff6">
+                <edge COLOR="#9dd169"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="Digital Wallets and Stores" FOLDED="false" POSITION="right" ID="53cc4f5c68a74ad790d9244d">
+                <edge COLOR="#9ed26a"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="Algorithm based systems" FOLDED="false" POSITION="right" ID="c3869ce0bb3b4bd2c318c8c8">
+                <edge COLOR="#9bd264"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="Gender based access to health" FOLDED="false" POSITION="right" ID="541c8333030a99b7a931a56f">
+                <edge COLOR="#9fd668"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+        </node>
+    </node>
+</map>
